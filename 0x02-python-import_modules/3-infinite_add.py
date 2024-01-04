f1df5ea5af428f5244da355e89ccb83e8b18ed58
AchrafElAlaoui <hraf23@gmail.com>
2024-01-04 23:19:16 +0000

    Update a file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-
-arguments = len(argv) - 1
-result = 0
-
-if arguments == 0:
+if __name__ == '__main__'
+    arguments = len(argv) - 1
     result = 0
-else:
-    for index in range(1, arguments + 1):
-        result += int(argv[index])
 
-print("{}".format(result))
+    if arguments == 0:
+        result = 0
+    else:
+        for index in range(1, arguments + 1):
+            result += int(argv[index])
 
+    print("{}".format(result))
