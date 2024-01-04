e80367bda816d9ab517339aad4b43a606bc0316a
AchrafElAlaoui <hraf23@gmail.com>
2024-01-04 00:23:39 +0000

    Update a file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first_digit in range(0, 9):
     for last_digit in range(first_digit + 1, 10):
         if first_digit == 8 and last_digit == 9:
-            print(f"{first_digit}{last_digit}")
+            print("{}{}".format(first_digit, last_digit))
         else:
-            print(f"{first_digit}{last_digit}", end=", ")
+            print("{}{}".format(first_digit, last_digit), end=", ")
