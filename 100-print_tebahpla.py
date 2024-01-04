4f1735deca1ae6b9a74fa0ad2a6f720fe4305ace
AchrafElAlaoui <hraf23@gmail.com>
2024-01-04 00:11:00 +0000

    Transfert to directory

--- 100-print_tebahpla.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-for ascii_character in range(122, 96, -1):
-    print("{}".format(chr(ascii_character) if ascii_character % 2 == 0
-          else chr(ascii_character - 32)), end="")
