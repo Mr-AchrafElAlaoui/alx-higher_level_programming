bb0f47399dbf63f5caf56fe96a1c95b5e674291d
AchrafElAlaoui <hraf23@gmail.com>
2024-01-04 23:14:52 +0000

    Update a file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import calculator_1 as calculator
 
-a = 10
-b = 5
+if __name__ == '__main__':
+    a = 10
+    b = 5
 
-print("{} + {} = {}".format(a, b, calculator.add(a, b)))
-print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
-print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator.div(a, b)))
+    print("{} + {} = {}".format(a, b, calculator.add(a, b)))
+    print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calculator.div(a, b)))
