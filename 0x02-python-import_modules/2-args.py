f0756d08dced66d05416890acc97e24715e2d755
AchrafElAlaoui <hraf23@gmail.com>
2024-01-04 23:34:25 +0000

    Update to respect pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
         print("{} arguments.".format(arguments))
 
     else:
-        print("{} {}".format(arguments, 'argument' if arguments == 1 
-              else 'arguments'))
+        print("{} {}".format(arguments, 'argument:' if arguments == 1
+              else 'arguments:'))
 
         for index in range(1, arguments + 1):
             print("{}: {}".format(index, argv[index]))
