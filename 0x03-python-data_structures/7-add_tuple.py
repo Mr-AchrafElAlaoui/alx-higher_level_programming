3b45835aeb8a6f4dc3b7bd018dc664a4d751ce92
AchrafElAlaoui <hraf23@gmail.com>
2024-01-08 19:32:11 +0000

    Update to respect pycodestyle

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,8 +11,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = tuple_a[:2]
 
     if len_tuple_b < 2:
-        tuple_b += (0,) * (2 -len_tuple_b)
+        tuple_b += (0,) * (2 - len_tuple_b)
     else:
         tuple_b = tuple_b[:2]
 
-    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
