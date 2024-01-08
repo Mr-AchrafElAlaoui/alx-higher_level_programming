154116f03d9981991c91fc22d00b95f1499a2c3c
AchrafElAlaoui <hraf23@gmail.com>
2024-01-08 15:51:50 +0000

    Add 2-replace_in_list.py file

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """Function that replaces an element of a list at a specific position"""
+    len_my_list = len(my_list)
+
+    if idx < 0 or idx >= len_my_list:
+        return None
+
+    else:
+        my_list[idx] = element
+        return my_list
