43acf1c35e94c093b4c334690ac0013cc0913fc4
AchrafElAlaoui <hraf23@gmail.com>
2024-01-08 18:56:46 +0000

    Update 11-delete_at.py file

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,6 +6,6 @@ def delete_at(my_list, idx=0):
 
     if len_my_list < 0 or len_my_list <= idx:
         return my_list
-    else:
-        del my_list[idx]
-        return my_list
+
+    del my_list[idx]
+    return my_list
