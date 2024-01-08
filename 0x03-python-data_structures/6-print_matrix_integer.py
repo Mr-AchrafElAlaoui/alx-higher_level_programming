f17e7773b61349eda405baac5005c2f9b7014dce
AchrafElAlaoui <hraf23@gmail.com>
2024-01-08 19:51:45 +0000

    Update to respect pycodestyle

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     """Function that prints a matrix of integers"""
     for row in matrix:
         for index in range(len(row)):
-            if index != len(row) - 1:
-                print("{}".format(row[index]), end=" ")
-            else:
+            if index == len(row) - 1:
                 print("{}".format(row[index]), end="")
+            else:
+                print("{}".format(row[index]), end=" ")
         print()
