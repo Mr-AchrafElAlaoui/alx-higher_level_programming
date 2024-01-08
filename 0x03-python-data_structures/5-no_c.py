8e13913e148c90af11be713e0fcbc0f7e9bcc3c8
AchrafElAlaoui <hraf23@gmail.com>
2024-01-08 16:29:56 +0000

    Add 5-no_c.py file

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_string = ""
+    for character in my_string:
+        if character == 'c' or character == 'C':
+            continue
+
+        new_string += character
+
+    return new_string
