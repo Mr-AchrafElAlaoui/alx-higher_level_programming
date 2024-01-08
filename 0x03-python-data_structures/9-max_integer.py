70987baa9681da8d41a500967d67a9216e4bb575
AchrafElAlaoui <hraf23@gmail.com>
2024-01-08 19:01:41 +0000

    Add 9-max_integer.py file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    """Function that finds the biggest integer of a list"""
+    if not my_list:
+        return None
+    else:
+        my_list.sort(reverse = True)
+        return my_list[0]
+
