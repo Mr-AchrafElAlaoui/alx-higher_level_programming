678b9ff7e375e556a07f8402483bca7b2743b078
AchrafElAlaoui <hraf23@gmail.com>
2024-01-11 00:21:15 +0000

    Add 100-weight_average.py file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        total_score = sum(map(lambda x: x[0] * x[1], my_list))
+        total_weight = sum(map(lambda x: x[1], my_list))
+        if total_weight == 0:
+            return 0
+        else:
+            return total_score / total_weight
