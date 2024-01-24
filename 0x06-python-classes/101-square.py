62e29f68f14d9e6be7987560739095c30515c01b
AchrafElAlaoui <hraf23@gmail.com>
2024-01-24 23:42:16 +0000

    Update the code to respect pycodestyle

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -35,7 +35,8 @@ class Square:
             str: A string representation of the square.
         """
         if self.__size == 0:
-            print()
+            print("")
+            return
         else:
             square_repre = "\n" * self.__position[1]
             square_repre += "\n".join([" " * self.__position[0] +
