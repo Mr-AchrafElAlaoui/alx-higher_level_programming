f0e78324b28e18237c952a06c9d2508b419bbb91
AchrafElAlaoui <hraf23@gmail.com>
2024-01-24 00:22:58 +0000

    Update the code to respect pycodestyle

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,17 +4,18 @@
 
 
 class Square:
-    """Square class with a private instance attribute size."""
+    """Square class with a private instance attribute size.
+
+    Attributes:
+        __size (int): The private instance attribute
+            representing the size of the square.
+    """
     def __init__(self, size=0):
         """
         Initializes a new instance of the Square class.
 
         Args:
             size (int, optional): The size of the square. Defaults to 0.
-
-        Attributes:
-            __size (int): The private instance attribute 
-                representing the size of the square.
 
         Raises:
             TypeError: If size is not an integer.
