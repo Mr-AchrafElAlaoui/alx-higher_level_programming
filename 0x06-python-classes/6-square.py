d85df5f75e04424b067152ed026bf8b2d88de999
AchrafElAlaoui <hraf23@gmail.com>
2024-01-24 16:57:38 +0000

    Update the code to respect pycodestyle

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -60,7 +60,7 @@ class Square:
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
-                len(value) != 2 or
+                not len(value) == 2 or
                 not all(isinstance(element, int) for element in value) or
                 not all(element >= 0 for element in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -78,11 +78,14 @@ class Square:
     def my_print(self):
         """Method that prints the square with '#' character.
         """
-        for new_line in range(self.__position[1]):
-            print("")
+        if self.__size == 0:
+            print()
+        else:
+            for new_line in range(self.__position[1]):
+                print("")
 
-        for _ in range(self.__size):
-            print(" " * self.__position[0], end="")
             for _ in range(self.__size):
-                print("#", end="")
-            print("")
+                print(" " * self.__position[0], end="")
+                for _ in range(self.__size):
+                    print("#", end="")
+                print("")
